bc98475d79300b8ad9c76bf706a5d007715ee5dc
bakhtiyarTayir <bakhtiyarTayir@gmail.com>
2023-07-12 15:41:54 +0500

    methods & computed

--- resources/js/components/PostComponent.vue
+++ resources/js/components/PostComponent.vue
@@ -1,13 +1,44 @@
 <template>
-    <div>
+    <div class="container-sm m-5 p-4">
+        <div class="row">
+            <div class="col-md-4">
+                <ul class="list-group">
+                    <li class="list-group-item">Name: {{ name }}</li>
+                    <li class="list-group-item">Age: {{ age }}</li>
+                    <li class="list-group-item">Job: {{ getJob }}</li>
+                </ul>
+            </div>
+        </div>
+        <button class="btn btn-primary mt-3" @click="sayHello">Hello</button>
+        <div class="mb-5"></div>
         <SinglePostComponent></SinglePostComponent>
     </div>
+
 </template>
 
 <script>
 import SinglePostComponent from "@/components/SinglePostComponent.vue";
 export default {
     name: "PostComponent",
+    data() {
+        return {
+            name: 'Roman',
+            age: 34,
+        }
+    },
+    methods: {
+        sayHello(){
+            alert('Hello!')
+        }
+    },
+    computed: {
+        getJob(){
+            return this.name + ' работает в булочной'
+        }
+    },
+
+
+
     components: {
         SinglePostComponent
     }
